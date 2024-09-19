836f95f4e305b7e6b1a189b3866bcc764fe964ad
Artjom Buzak <Atilusemi@gmail.com>
2024-09-19 21:00:41 +0300

    HwIter commited m

--- RequestsPy/req.py
+++ RequestsPy/req.py
@@ -1,7 +1,9 @@
+import html
 from http.client import responses
 
 import requests
 from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup as BS
 
 # 200 - success
 # 300 - redirect
@@ -19,3 +21,23 @@ except HTTPError as err:
     print(f'HTTP error: {err}')
 else:
     print('success')
+
+
+url = 'https://www.gammatest.net/ru/kurs_python_105.php'
+response = requests.get(url, timeout=20)
+soup = BS(response.content, 'html.parser')
+print(soup.title.text)
+match = soup.find('div', id='fb-root')
+match = soup.find_all('a')
+print(match)
+for m in match:
+    print(m.get_attribute_list('href'))
+
+row = match.findParent()
+
+response = requests.get('https://jsonplaceholder.typicode.com/users', timeout=20) #также работают ost get delete  и тд
+result = response.json()
+for post in result:
+    print(post['title'])
+    print(post['body'])
+    print('*' * 20)
