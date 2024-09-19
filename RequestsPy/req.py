d6b1d7de620a5c45f6211a93699479c2605ab80b
Artjom Buzak <Atilusemi@gmail.com>
2024-09-19 19:26:49 +0300

    HwIter commited m

--- RequestsPy/req.py
+++ RequestsPy/req.py
@@ -1,1 +1,21 @@
-import antigravity
+from http.client import responses
+
+import requests
+from requests.exceptions import HTTPError
+
+# 200 - success
+# 300 - redirect
+# 400 - client error
+# 500 - server error
+
+# r = requests.get('https://xkcd.com/353/', timeout=20)
+# print(r.cookies)
+
+
+try:
+    response = requests.get('http://github.com', timeout=20)
+    response.raise_for_status()
+except HTTPError as err:
+    print(f'HTTP error: {err}')
+else:
+    print('success')
