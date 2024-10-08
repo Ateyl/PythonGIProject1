73eb9e04829b144df46bcc41f5fabd7372d0c4d2
Artjom Buzak <Atilusemi@gmail.com>
2024-10-08 14:34:11 +0300

    server flask

---
+++ Flask/hashes.py
@@ -0,0 +1,3 @@
+import hashlib
+password = 'hello world'
+print(hashlib.md5(password.encode()).hexdigest())
--- Flask/server.py
+++ Flask/server.py
@@ -1,6 +1,15 @@
 from flask import Flask, redirect, url_for, render_template, request
+from datetime import timedelta
+import hashlib
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'hello'
+app.permanent_session_lifetime = timedelta(seconds=10)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/flask_python15gi'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 @app.route('/')
 def home():
@@ -14,18 +23,26 @@ def home():
 def login():
     if request.method == 'POST':
         user_name = request.form['user-name']
-        user_pass = request.form['user-pass']
+        user_pass = hashlib.md5(request.form['user-pass'].encode()).hexdigest()
         return redirect(url_for('user', name=user_name))
     else:
         return render_template('login.html')
 
-@app.route('/<name>')
+@app.route('/<name>/')
 def user(name):
     return f'Hello {name}'
 
-@app.route('/admin')
+
+@app.route('/admin/')
 def admin():
     return redirect(url_for('home'))
+
+
+@app.route('/logout/')
+def logout():
+    session.pop('login', None)
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
