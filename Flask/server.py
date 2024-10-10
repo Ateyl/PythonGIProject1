929d09a7f9af6fb53f5ab10df810f9458e2c10e8
Artjom Buzak <Atilusemi@gmail.com>
2024-10-10 16:11:04 +0300

    server flask mid commit

--- Flask/server.py
+++ Flask/server.py
@@ -1,36 +1,58 @@
+
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
+from sqlalchemy import desc
+from datetime import timedelta, datetime
 import hashlib
 from flask_sqlalchemy import SQLAlchemy
+from utils.helpers import allowed_file, unique_filename
 
 
 app = Flask(__name__)
 app.secret_key = 'hello'
-app.permanent_session_lifetime = timedelta(seconds=30)
+app.permanent_session_lifetime = timedelta(minutes=30)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/flask_python15gi'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config['UPLOAD_FOLDER'] = 'static/upload'
+app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
+
+
 db = SQLAlchemy(app)
+
+class Post(db.Model):
+    # __tablename__ = 'user-post'
+    _id_ = db.Column('post_id', db.Integer, primary_key=True)
+    title = db.Column('title', db.String(100))
+    content = db.Column('content', db.String(1000))
+    date_added = db.Column('date_added', db.DateTime())
+    owner = db.Column('owner', db.String(20))
+    def __init__(self, title, content, owner):
+        self.title = title
+        self.content = content
+        self.owner = owner
+        self.date_added = datetime.now()
 
 class User(db.Model):
     _id = db.Column('user_id', db.Integer, primary_key=True)
     login = db.Column('login',db.String(20), unique=True)
     password = db.Column('password', db.String(32))
     email = db.Column('email', db.String(100))
+    avatar = db.column('avatar', db.String(100))
 
-    def __init__(self, login, password, email):
+    def __init__(self, login, password, email='', avatar=''):
         self.login = login
         self.password = password
         self.email = email
+        self.avatar = avatar
 
 @app.route('/')
 def home():
-    print(url_for('home'))
+    posts = Post.query.order_by(desc('date_added')).all()
     return render_template(
         'index.html',
-        content='Random content',
-        people=['Jack', 'Mary', 'Bob'])
+        posts=posts
+    )
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -106,6 +128,30 @@ def delete_user():
     flash('User was deleted', 'success')
     return redirect(url_for('home'))
 
+@app.route('/post/', methods=['GET', 'POST'])
+def post():
+    if 'login' in session:
+        if request.method == 'POST':
+            title = request.form['post-title']
+            content = request.form['post-content']
+            new_post = Post(title, content, session['login'])
+            db.session.add(new_post)
+            db.session.commit()
+            flash('Post saved', 'success')
+            return redirect(url_for('home'))
+        else:
+            return render_template('post.html')
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/delete_post/<post_id>')
+def delete_post(post_id):
+    post = Post.query.filter_by(_id_=post_id).first()
+    if 'login' in session and post.owner == session['login']:
+        Post.query.filter_by(_id_=post_id).delete()
+        db.session.commit()
+        flash('Post was deleted','success')
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
